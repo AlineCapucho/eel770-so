1f8259ad4d2824a08cb1763853b88d098fe7ff2b
Ananias <86498679+ananiasmachado@users.noreply.github.com>
2023-07-18 22:57:05 -0300

    Fixed some problems, but program still is not according to requisites

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
-build: gcc -std=c17 -pthread main.c -o main
+# build: gcc -std=c17 -pthread main.c -o main
 
-run: ./main (parâmetro 1 - N) (parâmetro 2 - Np) (parâmetro 3 - Nc)
+# run: ./main (parâmetro 1 - N) (parâmetro 2 - Np) (parâmetro 3 - Nc)
 
 # the compiler
 CC = gcc
 
 # compiler flags:
 #  -Wall turns on most, but not all, compiler warnings
-CFLAGS  = -Wall
+CFLAGS  = -Wall -pthread
 
 # the build target executable:
 MAIN = main
--- main.c
+++ main.c
@@ -11,8 +11,11 @@
 #include <time.h>
 #include <string.h>
 
+// Parametrization macros
 #define NUM_REINDEER 9
-#define NUM_ELVES 10
+#define NUM_ELVES 6
+#define NUM_ITERATIONS 2
+// Program must end when Santa Claus delivery presents NUM_ITERATIONS times
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t santa_cond = PTHREAD_COND_INITIALIZER;
@@ -21,34 +24,57 @@ pthread_cond_t elves_cond = PTHREAD_COND_INITIALIZER;
 
 int reindeer_ready = 0;
 int elves_waiting = 0;
+int iteration = 1;
+
+void prepareSleigh(void) {
+    printf("Santa Claus is preparing sleigh for the reindeers.\n");
+    // Simulate preparing sleigh
+    sleep(2);
+} 
+
+void getHitched(int id) {
+    printf("Reindeer %d hitched to the sleigh.\n", id);
+}
+
+void helpElves(void) {
+    printf("Santa Claus is helping elves.\n");
+    // Simulate helping elves
+    sleep(2);
+}
+
+void getHelp(int id) {
+    printf("Elf %d getting help from Santa Claus.\n", id);
+}
 
 void* santa_claus(void* arg) {
     while (1) {
         pthread_mutex_lock(&mutex);
         
-        while (!reindeer_ready && elves_waiting < 3) {
+        while (reindeer_ready != NUM_REINDEER && elves_waiting < 3) {
             pthread_cond_wait(&santa_cond, &mutex);
         }
         
-        if (reindeer_ready) {
-            printf("Santa Claus preparing sleigh for the reindeer.\n");
-            // Simulate preparing sleigh
-            sleep(2);
+        if (reindeer_ready == NUM_REINDEER) {
+            prepareSleigh();
             printf("Santa Claus is delivering gifts.\n");
             // Simulate delivering gifts
             sleep(2);
             printf("Santa Claus is back from delivering gifts.\n");
             reindeer_ready = 0;
             pthread_cond_broadcast(&reindeer_cond);
+            printf("------ Iteration %d Completed ------\n", iteration);
+            ++iteration;
+            // Ending program because Santa Claus deliveried presents NUM_ITERATIONS times
+            if (iteration == NUM_ITERATIONS+1) {
+                exit(0);
+            }
         } else {
             for (int i = 0; i < 3; ++i) {
-                printf("Santa Claus helping elves.\n");
-                // Simulate helping elves
-                sleep(2);
-                printf("Santa Claus is back from helping the elves.\n");
+                helpElves();
                 pthread_cond_broadcast(&elves_cond);
             }
-            elves_waiting = 0;
+            printf("Santa Claus is back from helping the elves.\n");
+            elves_waiting -= 3;
         }
         
         pthread_mutex_unlock(&mutex);
@@ -57,46 +83,51 @@ void* santa_claus(void* arg) {
 }
 
 void* reindeer(void* arg) {
-    int id = *(int*)arg;
-    sleep(rand() % 5 + 1); // Simulate time to get ready
-    
-    pthread_mutex_lock(&mutex);
-    printf("Reindeer %d returned from vacation.\n", id);
-    ++reindeer_ready;
-    
-    if (reindeer_ready == NUM_REINDEER) {
-        pthread_cond_signal(&santa_cond);
+    for (int i = 0; i < NUM_ITERATIONS; ++i) {
+        int id = *(int*)arg;
+        sleep(rand() % 5 + 1); // Simulate time to get ready
+        
+        pthread_mutex_lock(&mutex);
+        printf("Reindeer %d returned from vacation.\n", id);
+        ++reindeer_ready;
+        
+        if (reindeer_ready == NUM_REINDEER) {
+            pthread_cond_signal(&santa_cond);
+        }
+        
+        pthread_cond_wait(&reindeer_cond, &mutex); // Wait for Santa Claus
+        
+        getHitched(id);
+        pthread_mutex_unlock(&mutex);
+        
+        // Simulate delivering gifts
+        sleep(2);
     }
-    
-    pthread_cond_wait(&reindeer_cond, &mutex); // Wait for Santa Claus
-    
-    printf("Reindeer %d hitched to the sleigh.\n", id);
-    pthread_mutex_unlock(&mutex);
-    
-    // Simulate delivering gifts
-    sleep(2);
     
     return NULL;
 }
 
 void* elf(void* arg) {
-    int id = *(int*)arg;
-    
-    pthread_mutex_lock(&mutex);
-    printf("Elf %d needs help from Santa Claus.\n", id);
-    ++elves_waiting;
-    
-    if (elves_waiting == 3) {
-        pthread_cond_signal(&santa_cond);
-    } else {
-        pthread_cond_wait(&elves_cond, &mutex);
+    for (int i = 0; i < NUM_ITERATIONS; ++i) {
+        int id = *(int*)arg;
+        sleep(rand() % 5 + 1); // Simulate time to need help
+        
+        pthread_mutex_lock(&mutex);
+        printf("Elf %d needs help from Santa Claus.\n", id);
+        ++elves_waiting;
+        
+        if (elves_waiting == 3) {
+            pthread_cond_signal(&santa_cond);
+        } else {
+            pthread_cond_wait(&elves_cond, &mutex);
+        }
+        
+        getHelp(id);
+        pthread_mutex_unlock(&mutex);
+        
+        // Simulate getting help
+        sleep(2);
     }
-    
-    printf("Elf %d getting help from Santa Claus.\n", id);
-    pthread_mutex_unlock(&mutex);
-    
-    // Simulate getting help
-    sleep(2);
     
     return NULL;
 }
